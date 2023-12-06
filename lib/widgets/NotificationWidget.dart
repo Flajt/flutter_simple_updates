8c6d3b45e949c9add4018be83e2715f4907954b2
Flajt <matrimkurz@gmail.com>
2023-12-06 16:57:23 +0100

    Feat: Add way to parse feedprovider to get feed data

--- lib/widgets/NotificationWidget.dart
+++ lib/widgets/NotificationWidget.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_updates/widgets/FeedDialog.dart';
+import 'package:flutter_simple_updates/abstract/AFeedParsingService.dart';
 
 class NotificationWidget extends StatelessWidget {
   final Icon icon;
+  final AFeedParsingService feedProvider;
   const NotificationWidget(
-      {super.key, this.icon = const Icon(Icons.notifications)});
+      {super.key,
+      this.icon = const Icon(Icons.notifications),
+      required this.feedProvider});
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (context) => const FeedDialog(
-                    feedEntries: [],
-                  ));
-        },
-        icon: icon);
+    return FutureBuilder(
+      future: feedProvider.parseFeed(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData && snapshot.data != null) {
+          final data = snapshot.data!;
+          return IconButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => FeedDialog(
+                          feedEntries: data,
+                        ));
+              },
+              icon: icon);
+        } else if (snapshot.hasError) {
+          return const Icon(Icons.warning);
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
+    );
   }
 }
